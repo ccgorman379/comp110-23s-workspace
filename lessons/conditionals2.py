65f6404003d3e520cabfe2ae815ae53522fd42f9
Cat Gorman <ccgorman@unc.edu>
2023-01-21 14:26:46 -0500

    lessons info

---
+++ lessons/conditionals2.py
@@ -0,0 +1,14 @@
+"""Knock knock joke with conditionals"""
+
+inter_cow = str = input("Do you want an interrupting cow? ")
+
+print("Knowck knock")
+if (inter_cow == "yes"):
+        print("...who's there?")
+        print("Interrupting cow.")
+        print("...interrupting cow wh---")
+        print("MOO!!!")
+else:
+        print("Oh...okay... :(") 
+        if(inter_cow == "you're not funny"):
+                print("Wow... that hurts my feelings...")     
