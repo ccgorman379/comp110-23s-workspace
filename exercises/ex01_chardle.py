affc272ef49da5ff70fc6e81635b9dfed8f626a1
Cat Gorman <ccgorman@unc.edu>
2023-01-22 20:44:21 -0500

    Take 3 Chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,11 +1,10 @@
-"""EX01 - Chardle Assignment - En Route to Wordle"""
+"""EX01 - Chardle Assignment - En Route to Wordle."""
 
-_author_ = "730556314"
+__author__ = "730556314"
 word = input("Enter a 5-character word: ")
-if(len(word) != 5):
+if(len(word) != 5 ):
         print("Error: Word must contain 5 characters ")
         exit()
-
 character = input("Enter a single character: ")
 if(len(character) != 1):
         print("Error: Character must be a single character. ")
@@ -29,12 +28,9 @@ if(character == word[3]):
 if(character == word[4]):
         print(character + " found at index 4")
         number_matching_characters = number_matching_characters + 1
-
 if(number_matching_characters == 1):
         print(str(number_matching_characters) + " instance of " + character + " found in " + word)
-
 if(number_matching_characters > 1):
         print(str(number_matching_characters) + " instances of " + character + " found in " + word)
-
 if(number_matching_characters == 0):
         print("No instances of " + character + " found in " + word)
