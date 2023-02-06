de0d671dcb48d734a8f6680235c618db5888f52b
Cat Gorman <ccgorman@unc.edu>
2023-02-06 15:29:33 -0500

    completed one shot wordle

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,16 +3,32 @@
 __author__ = "730556314"
 guess = input("What is your 6-letter guess? ")
 SECRET = str("python")
-while len(guess) != 6:
-    guess = input("That was not 6 letters! Try again: ")
-if (len(guess)) == 6:
-    if guess == str(SECRET):
-        print("Woo! You got it!")
-    else:
-        print("Not quite. Play again soon!")
-#part 2
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-SECRET_indx: int = 0
 guess_idx: int = 0
+result: str = ""
+while len(guess) != len(SECRET):
+    guess = input("That was not " + str(len(SECRET)) +  " letters! Try again: ")
+if (len(guess)) == len(SECRET):
+    while guess_idx < len(SECRET):
+        if guess[guess_idx] == SECRET[guess_idx]:
+            result = result + GREEN_BOX
+        else:
+            alt_location = False
+            alt_idx: int = 0
+            while not(alt_location == True) and (alt_idx < len(SECRET)):
+                if SECRET[alt_idx] == guess[guess_idx]:
+                    alt_location = True
+                else:
+                    alt_idx = alt_idx + 1
+            if alt_location == True:
+                result = result + YELLOW_BOX
+            else:
+                result = result + WHITE_BOX
+        guess_idx = guess_idx + 1
+    print(result)
+if guess == SECRET:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
