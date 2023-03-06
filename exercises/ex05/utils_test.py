523e2ebba99db9791125265e87e4b734dd49d1aa
Cat Gorman <ccgorman@unc.edu>
2023-03-06 12:29:33 -0500

    FINAL

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -4,37 +4,55 @@ __author__ = "730556314"
 
 from exercises.ex05.utils import only_evens, sub, concat
 
-def test_empty() -> None:
+
+def test_empty_1() -> None:
+    """Testing only_evens with an empty list input."""
     assert only_evens([]) == []
 
-def test_one_element() -> None:
-    test_list: list[int] = [2]
-    assert only_evens(test_list) == []
 
-def test_many() -> None:
+def test_one_element_1() -> None:
+    """Testing only_evens with a list with one element."""
+    test_list: list[int] = [2]
+    assert only_evens(test_list) == [2]
+
+
+def test_many_1() -> None:
+    """Testing only_evens with a list with multiple elements."""
     test_list: list[int] = [1, 2, 3]
     assert only_evens(test_list) == [2]
 
-def test_empty() -> None:
-    assert concat([],[]) == []
 
-def test_one_element() -> None:
+def test_empty_2() -> None:
+    """Testing concat with two empty list inputs."""
+    assert concat([], []) == []
+
+
+def test_one_element_2() -> None:
+    """Testing concat with two lists each with one element."""
     test_list_1: list[int] = [1]
     test_list_2: list[int] = [2]
     assert concat(test_list_1, test_list_2) == [1, 2]
 
-def test_many() -> None:
+
+def test_many_2() -> None:
+    """Testing concat with two lists each with multiple elements."""
     test_list_1: list[int] = [1, 2, 3]
     test_list_2: list[int] = [4, 5, 6]
     assert concat(test_list_1, test_list_2) == [1, 2, 3, 4, 5, 6]
 
-def test_empty() -> None:
+
+def test_empty_3() -> None:
+    """Testing sub with an empty list input and idices at zero."""
     assert sub([], 0, 0) == []
 
-def test_one_element() -> None:
+
+def test_one_element_3() -> None:
+    """Testing sub with a list with one element."""
     test_list: list[int] = [1]
     assert sub(test_list, 0, 0) == []
 
-def test_many() -> None:
+
+def test_many_3() -> None:
+    """Testing sub with a list with multiple elements."""
     test_list: list[int] = [1, 2, 3, 4, 5, 6]
     assert sub(test_list, 1, 3) == [2, 3]
