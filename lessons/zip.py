8682329c823e76fde121a7249d65f4be66a9b347
Cat Gorman <ccgorman@unc.edu>
2023-03-20 12:28:33 -0400

    cq04 final

--- lessons/zip.py
+++ lessons/zip.py
@@ -5,8 +5,14 @@ def zip(list_1: list[str], list_2: list[int]) -> dict[str, int]:
     result: dict[str, int] = {}
     if len(list_1) != len(list_2) or len(list_1) == 0 or len(list_2) == 0:
         return dict(result)
-    idx: int = 0
-    while len(list_1) < idx and len(list_2) < idx:
-        result[list_1(idx)] = list_2(idx)
-        idx += 1
-    return dict(result)
+    i = 0
+    for item in list_1:
+        result[item] = list_2[i] 
+        i += 1
+    print(result)
+    return result
+
+
+zip(list_1 = ["hi", "hello", "good"], list_2 = [1, 2, 3])
+
+    
