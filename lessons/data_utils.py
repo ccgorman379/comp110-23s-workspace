4e8089bf4b13343c49232f625a48ee4f230280b3
Cat Gorman <ccgorman@unc.edu>
2023-03-29 23:07:47 -0400

    LS23 work

--- lessons/data_utils.py
+++ lessons/data_utils.py
@@ -1,7 +1,7 @@
 from csv import DictReader
 
 def read_csv_rows(filename: str) -> list[dict[str,str]]:
-    """Read csv fule and return as list of dicts with header keys"""
+    """Read csv file and return as list of dicts with header keys"""
     result: list[dict[str,str]] = []
     file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
